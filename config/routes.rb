0e11634a554cac632db899946800a3e448c4dcae
krisike <59537435+krisike@users.noreply.github.com>
2020-05-17 21:09:41 -0500

    Initialize enhancement-14-adrian-homepage

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  get 'hello_world', to: 'hello_world#index'
+  resources :products
+  resources :welcome, only: [:index, :create]
+  resources :shopping_carts, only: [:show]
+  resources :orders, only: [:new, :create, :index, :show]
+  resource :shopping_cart_items, only: [:create]
+  resource :charges, only: [:new, :create]
+  root "welcome#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
